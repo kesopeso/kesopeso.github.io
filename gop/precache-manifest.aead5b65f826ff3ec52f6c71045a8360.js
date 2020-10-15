596020a9a3f23eb88dee18608d19c864b786844a
Dorde Kesic <dordekesic.keso@gmail.com>
2020-10-15 23:36:12 +0200

    adds docker env and a test build endpoint

---
+++ gop/precache-manifest.aead5b65f826ff3ec52f6c71045a8360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77cde919006379ad4e59a1e560271e0",
+    "url": "/gop/index.html"
+  },
+  {
+    "revision": "831b86c82e443ce12d3d",
+    "url": "/gop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c5d303222eed0789b2e",
+    "url": "/gop/static/js/2.7e6fd1ad.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gop/static/js/2.7e6fd1ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831b86c82e443ce12d3d",
+    "url": "/gop/static/js/main.299fce5a.chunk.js"
+  },
+  {
+    "revision": "2ad4f98ec69d914c5560",
+    "url": "/gop/static/js/runtime-main.f1347a37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gop/static/media/logo.5d5d9eef.svg"
+  }
+]);
